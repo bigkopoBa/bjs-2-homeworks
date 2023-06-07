2d704a203bacfe4b4a8ab505a255314236b83f57
Yurii Titov <titovich97@mail.ru>
2023-06-07 10:24:41 +0300

    Задание почти готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,33 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr;
+	let d = (Math.pow(b, 2)) - (4 * a * c);
+	if (d < 0) {
+		return arr = [];
+	}
+	if (d === 0) {
+		return arr = [-b / (2 * a)];
+	}
+	if (d > 0) {
+		return arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+	}
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let percentT, sum, result, toEqual = 0;
+	if (contribution >= amount) {
+		return 0;
+	} else {
+		if (countMonths <= 12) {
+			percentT = (percent / countMonths) / 100;
+		} else {
+			percentT = (percent / 12) / 100;
+		}
+		sum = amount - contribution;
+		result = sum * (percentT + (percentT / ((Math.pow((1 + percentT), countMonths)) - 1)));
+		toEqual = contribution + (result * countMonths);
+		console.log('Ввод: ' + percent + ', ' + contribution + ', ' + amount + ', ' + countMonths + '. Вывод: ' + (Math.floor(toEqual * 100) / 100))
+	}
+	return toEqual.toFixed(2);
 }
